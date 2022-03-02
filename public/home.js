79709d1ce1131029f7a6b548e55e99170857f0df
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-03-02 00:43:36 -0700

    completed about 60% of loans page

--- public/home.js
+++ public/home.js
@@ -7,13 +7,12 @@ button_div.addEventListener('mouseover', () => {
     if(window.innerWidth > 600){
         button.forEach(element => {
             element.style.color = 'white';
-            element.style.letterSpacing = '2.5px'
+            element.style.letterSpacing = '4px'
         })
-        button[1].style.fontSize = '2.55vw'
-        button_div.style.border = "3px solid #CC99CC"
-        button_div.style.borderRadius = "8px"
+        button[1].style.fontSize = '2.55vw';
+        button_div.style.borderRadius = "8px";
+        button_div.style.width = '10.5vw';
         button_div.style.backgroundColor = '#660066'
-        button_div.style.boxShadow = 'none'
     }
 })
 
@@ -25,8 +24,12 @@ button_div.addEventListener('mouseout', () => {
         element.style.letterSpacing = '0px';
     })
     button[1].style.fontSize = '2.5vw';
-    button_div.style.border = "3px solid black";
     button_div.style.backgroundColor = 'transparent';
     button_div.style.boxShadow = '2.5px -2.5px 2.5px #c8c8c8';
+    button_div.style.width = '8vw';
 }
+})
+
+button_div.addEventListener('click', () => {
+    location.href = './main.html';
 })
