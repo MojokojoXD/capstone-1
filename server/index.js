f5a1a3945c54a76c99d54cb6cfcc586ef9e4342b
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-03-05 16:50:15 -0700

    test on apple devices

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 const app = express();
 const port = process.env.PORT || 5000
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 
 app.get('/', (req,res) => {
