63b22d16f33bda2352441ec8513ec2553f7821f6
Kojo Boakye-Yiadom <kwadwoneer@gmail.com>
2022-03-14 14:57:53 -0600

    test config varible is heroku

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const seedPath = require('./seed')
 const path = require('path')
 const app = express();
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.use(express.json());
 // app.use(cors());
 
@@ -22,6 +22,6 @@ app.delete('/api/bank_trace',controller.rep_client_rm);
 app.get('/api/seed', seedPath.seed);
 app.get('/api/seedBank', seedPath.seedBanks);
 
-app.listen(port, () => {
-    console.log(`Port running on ${port}`)
+app.listen(PORT, () => {
+    console.log(`Port running on ${PORT}`)
 })
